dfe285a3463624caf5a8e00e7a56143956f1d370
Jonas Souza <jhonask25@gmail.com>
2023-06-04 22:29:49 -0300

    mobile commit

---
+++ app/index.tsx
@@ -0,0 +1,97 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, TouchableOpacity } from 'react-native';
+import * as SecureStore from 'expo-secure-store'
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import { useRouter } from "expo-router";
+
+import NLWlogo from '../src/assets/nlw-spacetime-logo.svg'
+import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
+import { useEffect } from 'react';
+import { api } from '../src/lib/api';
+
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/7eeea8620260444de2e2',
+};
+
+export default function App() {
+  const router = useRouter()
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  const [request, response, signInWithGitHub] = useAuthRequest(
+    {
+      clientId: '7eeea8620260444de2e2',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime'
+      }),
+    },
+    discovery,
+  );
+
+  async function handleGithubOAuhCode(code: string) {
+    const response = await api
+      .post('/register', {
+        code,
+      })
+      
+      const { token } = response.data
+
+      await SecureStore.setItemAsync('token', token)
+
+      router.push('/memories')
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      
+      handleGithubOAuhCode(code)
+    }
+  }, [response]);
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  return (
+    <View className='relative bg-gray-900 flex-1 items-center px-8 py-10'>
+       <View className='flex-1 items-center justify-center gap-6'>
+          <NLWlogo />
+        <View className='space-y-2'>
+        <Text className='text-center font-title text-2xl leading-tight text-gray-50'>Sua cápsula do tempo</Text>
+          <Text className='text-center font-body text-base leading-relaxed text-gray-100'>
+            Colecione momentos marcantes da sua jornada e compartilhe (se quiser)
+            com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+        activeOpacity={0.7}
+        className='rounded-full bg-green-500 px-5 py-2'
+        onPress={() => signInWithGitHub()}
+        >
+          <Text className='font-alt text-sm uppercase text-black'
+          >
+            Cadastrar lembrança</Text>
+        </TouchableOpacity>
+        </View>
+
+        <Text
+        className='text-center font-bold text-sm leading-relaxed text-gray-200'
+        >Feito com 💟 no NLW da Rocketseat</Text>
+
+      <StatusBar style="light" translucent />
+    </View>
+  );
+}
+
